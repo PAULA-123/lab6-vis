db68c63279393257a07e38de9808e0c1f0d3a039
paula lima <paulaeduardalima09@gmail.com>
2025-04-03 11:41:03 -0300

    Modificação

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,11 +1,14 @@
 <script>
     import projects from "$lib/projects.json";
     import Project from "$lib/Project.svelte";
+    import Pie from '$lib/Pie.svelte';
 </script>
 
 <svelte:head>
   <title>Projects</title>
 </svelte:head>
+
+<Pie/>
 
 <h1>{ projects.length } Projects</h1>
 <div class="projects">
